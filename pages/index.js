58c1805a6d8c43cacf45ba01a1a881f2c3f8dccc
FAROOQ AHAMED <72145702+farooq-alt@users.noreply.github.com>
2022-08-17 00:10:30 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+       I am currently pursuing Ph.D in Computer Science Engineering, currently working as a scientist SAC and held the position of the student ambassador (Educational and Research) of Computer Society of India and am also a Member of IEEE. I have played an integral role in organizing more than 4 International Conferences in India as the Organizing Chair and edited more than 80 Book Volumes from Springer LNCS, AISC. I am active in research in the areas of Quantum computing, Machine Learning, extraterrestrial learning and Cognitive Sciences. I have developed two new optimization algorithms known as Social Group Optimization (SGO), published in Springer Journal, I have delivered a number of Keynote addresses and Tutorials in my areas of expertise in various events in India. I have more than 15 publications in the field computing and physical sciences. I was awarded the couple wards in Leadership, Academia Award and 3 national and international awards in field of research. current ptojects parellel computing and propeller systems  <code>pages/index.js</code>
         </p>
       </main>
 
